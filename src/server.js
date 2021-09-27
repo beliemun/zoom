d1dd03f2e38b7ac51b87c6f5da68210013497473
beliemun <beliemun@paran.com>
2021-09-27 21:27:31 +0900

    #1.6 Chat Completed

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
-import express, { application } from "express";
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
-
-console.log("Hello~!");
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000 ✅");
+// app.listen(3000, handleListen);
+
+const httpServer = http.createServer(app);
+const wsServer = new WebSocket.Server({ server: httpServer });
+
+const onCloseSocket = () => console.log("Disconnected to Browser ❌")
+
+const sockets = [];
+
+wsServer.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", onCloseSocket);
+    socket.on("message", (message) => sockets.forEach(s => s.send(message)));
+})
+
+httpServer.listen(3000, handleListen);
