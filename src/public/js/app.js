33acc93834799ff591a94eaa78cbb67231ed381a
beliemun <beliemun@paran.com>
2021-09-28 14:32:59 +0900

    #1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,36 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+const makeMessage = (type, payload) => JSON.stringify({ type, payload })
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅")
 })
 
 socket.addEventListener("message", async (message) => {
-    console.log("message: ", await message.data.text())
+    const li = document.createElement("li");
+    li.innerHTML = await message.data.text();
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected to Server ❌")
 })
 
-const handleSubmit = (event) => {
+const handleNicknameSubmit = (event) => {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+}
+
+const handleMessageSubmit = (event) => {
     event.preventDefault();
     const input = messageForm.querySelector("input");
     socket.send(input.value);
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
+messageForm.addEventListener("submit", handleMessageSubmit);
